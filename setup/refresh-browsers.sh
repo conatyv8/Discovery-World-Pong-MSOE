ed5226eabc1812bc5f8fd77f934b0cdec27f3eae
Aaron Neustedter <ajneuste@ra.rockwell.com>
2024-05-14 17:20:17 -0500

    CI/CD (#100)

--- setup/refresh-browsers.sh
+++ setup/refresh-browsers.sh
@@ -6,7 +6,18 @@ windows=("Clocktower Visualizer" "Gameboard" "Human Visualizer" "Neural Network 
 
 for w in "${windows[@]}"
 do
-  echo "Refreshing $w"
-  xdotool windowactivate --sync $(xdotool search --name "$w")
-  xdotool key --window $(xdotool search --name "$w") F5 
+  # Get window id from window name
+  windowid=$(xdotool search --name "$w")
+  # Check return code, make sure the window is open
+  rc=$?
+  if [ $rc -eq 0 ]; then
+    echo "Refreshing $w"
+    xdotool windowactivate --sync $windowid 
+    xdotool key --window $windowid F5 
+  else
+    # Warn if window is not available. Also print out wanrning in GitHub 
+    # Actions warning syntax so that actions will pick it up
+    echo "Warning! Window $w not found"
+    echo ":warning file={setup/refresh-browsers.sh},line={10},title={Window Not Found for Refresh}::{Window $w was not found by xdotool. Is the window open?}"
+  fi
 done
