840b88f92fa5578d8e1e8c8cc60fc68b966cf807
Aaron Neustedter <ajneuste@ra.rockwell.com>
2025-05-22 12:49:32 -0500

    Add missing file

---
+++ modules/single-screen-development/src/Sidebar/CommandPusher/CommandsAndContainers.tsx
@@ -0,0 +1,15 @@
+export const commands : string[] = ["Start", "Stop", "Build", "Down", "Up"];
+export const containers : string[][] = [
+                                 //["mqtt-broker", "MQTT Broker"],
+                                 // ["mqtt-explorer", "MQTT Explorer"],
+                                ["ai-paddle-control", "AI Paddle Control"],
+                                ["game-engine", "Game Engine"],
+                                ["human-paddle-control", "Human Paddle Control"],
+                                ["human-visualizer", "Human Visualizer"],
+                                ["neural-net-visualizer", "Neural Net Visualizer"],
+                                ["clocktower-visualizer", "Clock Tower Visualizer"],
+                                ["gameboard", "Game board"],
+                                ["audio-engine", "Audio Engine"],
+                                //"single-screen-development",
+                                ["log-server", "Log Server"]
+                              ];
